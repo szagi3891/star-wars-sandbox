618f2d19ca299722f956e73f5a34a214b1616ced
Grzegorz Szeliga <szeligagrzegorz@gmail.com>
2019-06-21 06:10:15 +0200

    komentarz

--- src/AppState/api.ts
+++ src/AppState/api.ts
@@ -5,6 +5,17 @@ interface ListResult {
     }>
 };
 
+/*
+    Funkcja getListJson powinna mieć taki typ:
+
+    const getListJson = async (
+        url: string,
+        decode: (data: unknown) => T | Error
+    ): Promise<T> => {
+
+    Ten parametr generyczny w obecnej wersji jest totalnie niepoprawny.
+*/
+
 const getListJson = async <T>(url: string): Promise<T> => {
     const result = await fetch(url);
     return await result.json();
