7495dec6b74db327f6a30a16e2ccb4d10b8adf5c
Grzegorz Szeliga <szeligagrzegorz@gmail.com>
2024-05-10 06:18:16 +0200

    counter

--- src/View/FilmList.tsx
+++ src/View/FilmList.tsx
@@ -2,6 +2,21 @@ import * as React from 'react';
 import { FilmLabel } from './FilmLabel';
 import { observer } from 'mobx-react-lite';
 import { FilmIdModel } from '../AppState/ models/FilmIdModel';
+import { createAutoMap } from '../utils/createMap';
+import { makeObservable, observable } from 'mobx';
+
+class ListState {
+    @observable counter: number = 0;
+
+    public static get = createAutoMap(() => new ListState());
+    private constructor() {
+        makeObservable(this);
+    }
+
+    inc = () => {
+        this.counter += 1;
+    }
+}
 
 interface PropsType {
     films: Array<FilmIdModel>
@@ -16,6 +31,13 @@ export const FilmList = observer((props: PropsType) => {
     return (
         <>
             { list }
+            <div>
+                current counter = {ListState.get().counter}
+            </div>
+            <div onClick={() => ListState.get().inc()}>
+                Up
+            </div>
+
         </>
     );
 });
